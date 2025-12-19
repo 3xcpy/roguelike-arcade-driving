57cc2b1755feb6beec09918b09a0bc05e6885d8c
3xcpy <3xcpyy@gmail.com>
2025-12-19 18:21:00 +0100

    sliding and traction is working, drifting should be next

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
 @export var speed: float = 200.0
+@export var accel: float = 100.0
 @export var steer_strength: float = 30.0
+@export var traction: float = 10.0
 
 @export var wheel_base: float = 16.0 # distance between the front and back wheel
 
+
 func _physics_process(delta: float) -> void:
+	if velocity.length() < speed:
+		velocity += transform.x * accel * delta
+
 	var steer = Input.get_axis("steer_left", "steer_right") * deg_to_rad(steer_strength)
-	velocity = transform.x * speed
 
 	var front_wheel: Vector2 = position + transform.x * (wheel_base/2.0)
 	var back_wheel: Vector2 = position - transform.x * (wheel_base/2.0)
@@ -17,6 +22,6 @@ func _physics_process(delta: float) -> void:
 
 	var new_dir = back_wheel.direction_to(front_wheel)
 
-	velocity = new_dir * velocity.length()
+	velocity = lerp(velocity, new_dir * velocity.length(), traction * delta)
 	rotation = new_dir.angle()
 	move_and_slide()
