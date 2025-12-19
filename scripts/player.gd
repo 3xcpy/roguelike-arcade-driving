5f9fc61649ee8243c89bed9e093584d98ec062f4
3xcpy <3xcpyy@gmail.com>
2025-12-19 16:42:56 +0100

    it's working now
    finally

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,19 +7,25 @@ extends CharacterBody2D
 
 @export var wheel_base: float = 16.0 # distance between the front and back wheel
 
+@onready var s1 = $Sprite2D2
+@onready var s2 = $Sprite2D3
+
 
 func _physics_process(delta: float) -> void:
 	var steer = Input.get_axis("steer_left", "steer_right") * steer_strength
+	velocity = transform.y * max_speed
 
-	var front_wheel: Vector2 = position + wheel_base/2.0 * Vector2.UP.rotated(rotation)
-	var back_wheel: Vector2 = position - wheel_base/2.0 * Vector2.UP.rotated(rotation)
+	var front_wheel: Vector2 = position + transform.x * (wheel_base/2.0)
+	var back_wheel: Vector2 = position - transform.x * (wheel_base/2.0)
 
-	front_wheel += max_speed * Vector2.UP.rotated(rotation + deg_to_rad(steer)) * delta
-	back_wheel += max_speed * Vector2.UP.rotated(rotation) * delta
+	front_wheel += velocity.rotated(deg_to_rad(steer)) * delta
+	back_wheel += velocity * delta
 
-	var new_location = (front_wheel + back_wheel) / 2
-	rotation = (front_wheel - back_wheel).angle()
-	position = new_location
+	s1.global_position = front_wheel
+	s2.global_position = back_wheel
 
-	# velocity = (new_location - position).normalized() * accel
+	var new_dir = back_wheel.direction_to(front_wheel)
+
+	velocity = new_dir * velocity.length()
+	rotation = new_dir.angle()
 	move_and_slide()
