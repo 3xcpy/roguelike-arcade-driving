8a7d383b148773c588974e60f09351104df02b61
3xcpy <3xcpyy@gmail.com>
2026-01-01 12:13:31 +0100

    add enemy movement

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,9 +2,29 @@ class_name Enemy extends Area2D
 
 @export var attack_damage: float = 1.0
 @export var value: int = 1
+@export var speed: float = 50.0
+@export var accel: float = 10.0
+@export var decel: float = 1.0
+@export var level: int = 1:
+	set(new_level):
+		level = new_level
+		if Engine.is_editor_hint():
+			update_values()
 
 var tick: float = 0.1
 var timer := 0.0
+var index := 0
+@export var update_amount: int = 10
+var update_timer: int = 0
+
+var velocity: Vector2 = Vector2.ZERO
+
+
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+
+
+func _ready() -> void:
+	index = Global.get_enemy_index()
 
 
 func _physics_process(delta: float) -> void:
@@ -17,6 +37,33 @@ func _physics_process(delta: float) -> void:
 			if b.is_in_group("Player"):
 				if b.has_method("damage"):
 					b.damage(attack_damage)
+	
+	if update_timer % update_amount == index % update_amount:
+		navigate()
+	update_timer += 1	
+
+	var dir = to_local(navigation_agent.get_next_path_position()).normalized()
+	var desired_vel = dir * speed
+	var new_vel = Vector2.ZERO
+
+	if velocity.length() < desired_vel.length():
+		new_vel = lerp(velocity, desired_vel, accel * delta)
+	else:
+		new_vel = lerp(velocity, Vector2.ZERO, decel * delta)
+	
+	global_position += velocity * delta
+
+	navigation_agent.set_velocity(new_vel)
+
+
+func navigate() -> void:
+	var player = get_tree().get_nodes_in_group("Player")[0]
+	navigation_agent.target_position = player.global_position
+
+
+func update_values() -> void:
+	scale = Vector2(level, level)
+	attack_damage = level
 
 
 func damage(dmg: float): $HealthComponent.damage(dmg)
@@ -30,3 +77,6 @@ func _on_health_component_dead() -> void:
 func _on_health_component_damaged(_dmg: float) -> void:
 	$DamageParticles.emitting = true
 	$DamageParticles.restart()
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
--- scripts/global.gd
+++ scripts/global.gd
@@ -2,3 +2,8 @@ extends Node
 
 var floor: Floor = null
 var game_manager: GameManager = null
+
+var current_index: int = 0
+func get_enemy_index() -> int:
+	current_index += 1
+	return current_index - 1
