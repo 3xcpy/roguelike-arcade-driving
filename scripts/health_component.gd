3d22fef671bb73b834afb97e01f93d9bb57bb04e
3xcpy <3xcpyy@gmail.com>
2026-01-30 18:12:28 +0100

    get started working on sfx

--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -5,13 +5,32 @@ signal healed(amount: float)
 signal dead
 
 @export var max_health: float = 10.0
+@export var hurt_sound = null
+@export var heal_sound = null
+
 var health: float = max_health
+
+var audio_playback: AudioStreamPlaybackPolyphonic
+
+func _ready() -> void:
+	if hurt_sound != null:
+		var asp = AudioStreamPlayer.new()
+		add_child(asp)
+
+		var stream = AudioStreamPolyphonic.new()
+		stream.polyphony = 16	
+		asp.stream = stream
+		asp.play()
+		audio_playback = asp.get_stream_playback()
 
 
 func damage(dmg: float) -> void:
 	health -= dmg
 	if health <= 0:
 		dead.emit()
+	else:
+		if hurt_sound != null:
+			audio_playback.play_stream(hurt_sound, 0, 0, randf_range(0.9, 1.1), AudioServer.PlaybackType.PLAYBACK_TYPE_DEFAULT, &"SFX")
 	damaged.emit(dmg)
 
 
@@ -19,4 +38,6 @@ func heal(amount: float) -> void:
 	health += amount
 	if health > max_health:
 		health = max_health
+	if heal_sound != null:
+		audio_playback.play_stream(heal_sound, 0, 0, randf_range(0.9, 1.1), AudioServer.PlaybackType.PLAYBACK_TYPE_DEFAULT, &"SFX")
 	healed.emit(amount)
