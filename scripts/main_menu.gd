60e09754e7230e93adc228d15869aca2b09814bb
3xcpy <3xcpyy@gmail.com>
2026-01-31 13:42:11 +0100

    web build

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,11 +3,6 @@ class_name MainMenu extends Control
 signal start_game
 
 
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		start_game.emit()
-
-
 func _on_quit_button_button_up() -> void:
 	get_tree().quit()
 
